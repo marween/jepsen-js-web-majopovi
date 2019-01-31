3e51915870b2a254f009c85394838e144b99c285
scalajeremy <scalajeremy@gmail.com>
2019-01-31 16:52:02 +0100

    showdown not working yet

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // import some polyfill to ensure everything works OK
 import "babel-polyfill"
 
@@ -12,4 +11,36 @@ import "./style.scss";
   Put the JavaScript code you want below.
 */
 
-console.log("Hey look in your browser console. It works!");
+console.log("coucou");
+
+//
+// function run() {
+//   var text = document.getElementById('sourceTA').value,
+//       target = document.getElementById('targetDiv'),
+//       converter = new showdown.Converter(),
+//       html = converter.makeHtml(text);
+//
+//     target.innerHTML = html;
+// }
+
+document.getElementById(runBtn).addEventListener("click", () => {
+  function run() {
+    var text = document.getElementById('sourceTA').value,
+        target = document.getElementById('targetDiv'),
+        converter = new showdown.Converter(),
+        html = converter.makeHtml(text);
+
+      target.innerHTML = html;
+  }
+})
+
+
+
+
+// document.querySelector("#butdiv").addEventListener("click", () => {
+//   var x = parseInt(document.getElementById("num1").value);
+//   var y = parseInt(document.getElementById("num2").value);
+//   var result = x / y;
+//   console.log("Résultat est " + result);
+//   document.getElementById("result").innerHTML = result;
+// });
