7810c0d6edae3cb93ba319c9ac4aa3f404465416
marween <annemagali.saintgeorges@gmail.com>
2019-01-31 16:35:35 +0100

    ajout commentaire +btn submit

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ import "./style.scss";
 */
 
 // Get the modal
-var modal = document.getElementById('myModal');
+let modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
@@ -36,5 +36,14 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+////////////////////////Magali/////////////////////////////
+// ajout un d'un texarea au click
+
+document.querySelector("#comment").addEventListener("click", () => {
+	let content = "";
+	document.querySelector("#comment").innerHTML = "<textarea> coucou"+content+"</textarea> <input type='submit' value='submit'>" ;
+});
+
 console.log("coucou");
 
