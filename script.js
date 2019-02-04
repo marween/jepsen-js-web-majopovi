9997fec6a409bf38809cc11ab7546d934563c8c4
Raigyo <vincent.chilot@gmail.com>
2019-02-04 21:35:00 +0100

    update

--- script.js
+++ script.js
@@ -27,6 +27,25 @@ let dataJson = [
     }
   ];
 
+  // Get the modal
+  let modal = document.getElementById('myModal');
+  // Get the button that opens the modal
+  let btn = document.getElementById("myBtn");
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    };
+  };
+
 let indexClicked;
 
 //adding 'ideas' on the landing page from object 'dataJson'
@@ -37,14 +56,13 @@ let list =  () => {
     str += "<li id='" + idNumber + "'>" + dataJson[i].idea + "</li>";
   };
   str += '</ul>';
-  document.getElementById("contentIdeas").innerHTML = str;
+  document.getElementsByClassName("ideas-list").innerHTML = str;
 };
 list ();
 
 //transform items 'ideas' into buttons
 let listToButons =  () => {
-    var lis=document.getElementById('listIdeas')
-    .getElementsByTagName('li');
+    let lis=document.getElementById('listIdeas').getElementsByTagName('li');
     for(let i = 0; i < lis.length; i++) {
         lis[i].onclick= function(){listToButonsAction(i);};
     }
@@ -53,10 +71,11 @@ listToButons ();
 
 //put an action on the item clicked
 function listToButonsAction (i) {
-    indexClicked = i;
-    modal.style.display = "block";//change with the new modal (jeremy)
-    document.getElementById("modTitle").innerHTML = dataJson[indexClicked].idea;
-    document.getElementById("modDescr").innerHTML = dataJson[indexClicked].description;
+    //indexClicked = i;
+    modal.style.display = "block";
+    //change with the new modal (jeremy)
+    document.getElementById("modTitle").innerHTML = dataJson[i].idea;
+    document.getElementById("modDescr").innerHTML = dataJson[i].description;
     //document.getElementById("modCom").innerHTML = dataJson[indexClicked].commentary;
     //alert(indexClicked+' has been clicked !');
 }
@@ -72,24 +91,7 @@ document.getElementById("delete-button").onclick = function() {
   modal.style.display = "none";
 }
 
-// Get the modal
-let modal = document.getElementById('myModal');
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  };
-};
 
 
 
