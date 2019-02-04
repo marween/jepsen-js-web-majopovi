717a636d96b9df497e92ae7a4995e29ef4417aeb
Raigyo <vincent.chilot@gmail.com>
2019-02-04 09:24:20 +0100

    update

--- script.js
+++ script.js
@@ -12,14 +12,42 @@ import "./style.scss";
   Put the JavaScript code you want below.
 */
 
+/** Local Storage with JavaScript **/
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.getElementById('item');
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
-document.getElementById("btnEdit").onclick = function() {
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
 
-};
+const liMaker = (text) => {
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
+}
 
-document.getElementById("btnRemove").onclick = function() {
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-};
+  itemsArray.push(input.value);
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  liMaker(input.value);
+  input.value = "";
+});
+
+data.forEach(item => {
+  liMaker(item);
+});
+
+button.addEventListener('click', function () {
+  localStorage.clear();
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  itemsArray = [];
+});
 
 //----------------------------------------
 
