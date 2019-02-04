44f9a4ff55f0b03387949448e69633e6c1d57394
scalajeremy <scalajeremy@gmail.com>
2019-02-04 20:27:00 +0100

    2 modals base structure

--- script.js
+++ script.js
@@ -8,22 +8,58 @@ import 'bootstrap';
 import "./style.scss";
 
 /*Modal Box*/
-// Get the modal
-let modal = document.getElementById('myModal');
+/* With buttons in html to open the right one for test purpos */
+// Get the Add modal
+var modal = document.getElementById('modal-add');
+// Get the button that opens the modal
+var btn = document.getElementById("modal-add-btn");
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("modal-add-close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-	modal.style.display = "none";
+  modal.style.display = "none";
 }
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
+// Get the Edit modal
+var modal2 = document.getElementById('modal-edit');
+// Get the button that opens the modal
+var btn2 = document.getElementById("modal-edit-btn");
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("modal-edit-close")[0];
+
+// When the user clicks the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+
+
 
 /* / Modal box */
 
