c9a4b30504ba7fc7f8c4ee7a80373d55a1bb455f
marween <annemagali.saintgeorges@gmail.com>
2019-02-05 23:48:14 +0100

    fin script

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ import "./style.scss";
 // Get the Add modal
 var modal = document.getElementById('modal-add');
 var modalEdit = document.getElementById('modal-edit');
+var modalComment = document.getElementById('modal-comment');
 // Get the button that opens the modal
 var btn = document.getElementById("modal-add-btn");
-var edit = document.querySelector('modal-edit-btn');
+var edit = document.querySelector('.modal-edit-btn');
+var display = document.querySelector('.modal-display-btn');
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("modal-add-close")[0];
 
@@ -49,7 +51,17 @@ window.onclick = function(event) {
   }
 }
 
+// Get the Comment modal
+var modalComment = document.getElementById('modal-comment');
+// Get the <span> element that closes the modal
+var spanModalComment = document.getElementsByClassName("modal-comment-close")[0];
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalComment) {
+    modalComment.style.display = "none";
+  }
+}
 
 //-----------------------fin des fct de la modal box----------------------------------
 // debut des fonctions de l'application 
@@ -91,7 +103,7 @@ on ajoute pour chaque idée un bouton delete edit et afficher */
 
 let displayIdeas = () => {
   //decommenter pour nettoyer le localstorage
-  // localStorage.setItem('content', JSON.stringify(dataJson));
+  //localStorage.setItem('content', JSON.stringify(dataJson));
   let listIdeas = localStorage.getItem('content') ? JSON.parse(localStorage.getItem('content')) : [];
   let toDisplay = "";
 
@@ -106,7 +118,7 @@ let displayIdeas = () => {
     editBtn.setAttribute("class", "modal-edit-btn");
     editBtn.innerText = "Edit";
 
-    // on edit 
+    // function edit
     editBtn.addEventListener('click', () => {
       document.querySelector("#edit-title").value = listIdeas[i].idea;
       document.querySelector("#edit-descr").innerText = listIdeas[i].description;
@@ -132,23 +144,64 @@ let displayIdeas = () => {
     deleteBtn.setAttribute("class", "modal-delete-btn");
     deleteBtn.innerText = "Delete";
 
-    // on supprime 
+    // function delete 
     deleteBtn.addEventListener('click', () => {
       listIdeas.splice(i, 1);
       localStorage.setItem('content', JSON.stringify(listIdeas));
       window.location.reload();
-
     }); // end of delete
     
-    // on affiche la liste avec le ul et li +  avec les boutons delete et edit 
+    
+  /*Pour chaque idée, on doit pouvoir donner un 
+  commentaire qui lui est propre*/
+    // creation du bouton Display
+    let displayBtn = document.createElement ("button");
+    displayBtn.setAttribute("class", "modal-display-btn");
+    displayBtn.innerText = "Display";
+
+    
+    // function Display
+    displayBtn.addEventListener('click', () => {
+
+      // fonction add commentaire
+      let input_textarea = document.querySelector('.modal-comment');
+      let pComments = document.querySelector('.modal-com');
+      let addCommentBtn = document.querySelector('.add-comment');
+      let listIdeas = localStorage.getItem('content') ? JSON.parse(localStorage.getItem('content')) : [];
+      input_textarea.value="";
+
+      console.log(JSON.parse(localStorage.getItem('content')));
+
+      //function addComment pour ajouter 
+      addCommentBtn.addEventListener('click', () => {
+        listIdeas[i].commentary.push(input_textarea.value);
+        localStorage.setItem('content', JSON.stringify(listIdeas));
+
+      }); //end addComment.btn
+      
+      // on affiche tous les commentaires d'une idée 
+      input_textarea.value="";
+      let toDisplay = "";
+      for (let j=0; j<listIdeas[i].commentary.length; j++ ){
+        toDisplay += "<p>" + listIdeas[i].commentary[j] + "</p>";
+      }// end of for
+      
+      //listIdeas[i].commentary = document.querySelector(".modal-comment").value;
+      pComments.innerHTML = toDisplay;
+      modalComment.style.display = "block";
+  }); //end displayBtn
+
+    // on affiche la liste avec le ul et li +  avec les boutons display, delete et edit 
     let ul =document.querySelector('ul');
     ul.appendChild(li);
     ul.appendChild(deleteBtn);
     ul.appendChild(editBtn);
+    ul.appendChild(displayBtn);
 
-  } // end of for
 
-}// end function displayIdeas
+
+  }; // end of for de la liste des idées
+};// en of displayIdeas-----------------
 
 
 /*Sur la page index, on doit pouvoir ajouter une nouvelle idée 
@@ -177,40 +230,6 @@ addBtn.addEventListener("click", () => {
   addIdea(newIdea, newDes);
 
 });// end of add 
-
-
-
-/*Pour chaque idée, 
-on doit pouvoir donner un 
-commentaire qui lui est propre*/
-
-
-// fonction qui ajoute un commentaire
-// passer en parametre le numero de l'idée
-
-let input_textarea = document.querySelector('.modal-comment');
-let output_div = document.querySelector('.modal-com');
-let save_button = document.querySelector('.save-idea');
-let listIdeas = localStorage.getItem('content') ? JSON.parse(localStorage.getItem('content')) : [];
-let listComments = listIdeas[1].commentary;
-let comment ="";
-
-let addComments = () => {
-
-  comment.push(input_textarea.value);
-
-  //localStorage.setItem('content', JSON.stringify(listIdeas));
-
-  input_textarea.value="";
-  let toDisplay = "";
-
-  for(let i=0; i<listComments.length; i++ ){
-
-    toDisplay += "<p>" + listComments[i] + "</p>";
-  }
-  output_div.innerHTML = toDisplay;
-
-};
 
 
 //--------------------------------------------------------------------------------------
