6deaa19112e504635b3488dc5fc100871a53b176
marween <annemagali.saintgeorges@gmail.com>
2019-02-06 10:55:02 +0100

    modif span

--- script.js
+++ script.js
@@ -13,12 +13,18 @@ import "./style.scss";
 var modal = document.getElementById('modal-add');
 var modalEdit = document.getElementById('modal-edit');
 var modalComment = document.getElementById('modal-comment');
+
 // Get the button that opens the modal
 var btn = document.getElementById("modal-add-btn");
 var edit = document.querySelector('.modal-edit-btn');
 var display = document.querySelector('.modal-display-btn');
+
+
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal-add-close")[0];
+var span = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close")[1];
+var span2 = document.getElementsByClassName("close")[2];
+
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
@@ -28,35 +34,33 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+
+}
+span1.onclick = function() {
+ 
+  modalEdit.style.display = "none";
+}
+span2.onclick = function() {
+ 
+  modalComment.style.display = "none";
 }
 
+
+
+
+  
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-
-
 }
-// Get the Edit modal
-var modal2 = document.getElementById('modal-edit');
-// Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("modal-edit-close")[0];
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
+  if (event.target == modalEdit) {
+    modalEdit.style.display = "none";
   }
 }
-
-// Get the Comment modal
-var modalComment = document.getElementById('modal-comment');
-// Get the <span> element that closes the modal
-var spanModalComment = document.getElementsByClassName("modal-comment-close")[0];
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modalComment) {
     modalComment.style.display = "none";
