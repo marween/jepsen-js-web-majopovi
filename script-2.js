cd88938605a5ea9ae46a6dd1389aeb7f9ed32e7c
Raigyo <vincent.chilot@gmail.com>
2019-02-06 11:19:34 +0100

    old

--- script-2.js
+++ script-2.js
@@ -7,112 +7,234 @@ import 'bootstrap';
 // import the style
 import "./style.scss";
 
-//check if there is something storage otherwise create an object as exemple
-//let dataJson = JSON.parse(localStorage.getItem('dataJson'));
+/*Modal Box*/
+/* With buttons in html to open the right one for test purpos */
+// Get the Add modal
+let modalAdd = document.getElementById('modal-add');
+let modalEdit = document.getElementById('modal-edit');
+let modalComment = document.getElementById('modal-comment');
+// Get the button that opens the modal
+let btnAdd = document.getElementById("modal-add-btn");
+let btnEdit = document.querySelector('.modal-edit-btn');
+let btnComment = document.querySelector('.modal-display-btn');
+// Get the <span> element that closes the modal
+let addClose = document.getElementsByClassName("modal-add-close")[0];
+let editClose = document.getElementsByClassName("modal-edit-close")[0];
+let commentClose = document.getElementsByClassName("modal-comment-close")[0];
+
+// When the user clicks the button, open the modal
+btnAdd.onclick = function() {
+  modalAdd.style.display = "block";
+}
+
+btnEdit.onclick = function() {
+  modalEdit.style.display = "block";
+}
+
+btnComment.onclick = function() {
+  modalComment.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalAdd) {
+    modalAdd.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modalEdit) {
+    modalEdit.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modalComment) {
+    modalComment.style.display = "none";
+  }
+}
+
+//-----------------------fin des fct de la modal box----------------------------------
+// debut des fonctions de l'application
+
 let dataJson = [
-    {
-      "idea": "Test: Idea 1",
-      "description": "Idea 1 description",
-      "commentary": []
-    },
-    {
-      "idea": "Test: Idea 2",
-      "description": "Idea 2 description",
-      "commentary": []
-    },
-    {
-      "idea": "Test: Idea 3",
-      "description": "Idea 3 description",
-      "commentary": []
-    }
-  ];
-
-  // Get the modal
-  let modal = document.getElementById('myModal');
-  // Get the button that opens the modal
-  let btn = document.getElementById("myBtn");
-  // Get the <span> element that closes the modal
-  let span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  };
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    };
-  };
-
-let indexClicked;
-
-//adding 'ideas' on the landing page from object 'dataJson'
-let list =  () => {
-  let str = "<ul id='listIdeas'>";
-  for (let i in dataJson) {
-    let idNumber = "idea"+i;
-    str += "<li id='" + idNumber + "'>" + dataJson[i].idea + "</li>";
-  };
-  str += '</ul>';
-  document.getElementsByClassName("ideas-list").innerHTML = str;
-};
-list ();
-
-//transform items 'ideas' into buttons
-let listToButons =  () => {
-    let lis=document.getElementById('listIdeas').getElementsByTagName('li');
-    for(let i = 0; i < lis.length; i++) {
-        lis[i].onclick= function(){listToButonsAction(i);};
-    }
+{
+  "id": "0",
+  "idea": "Test: Idea 1",
+  "description": "Idea 1 description",
+  "commentary": []
+},
+{
+  "id": "1",
+  "idea": "Test: Idea 2",
+  "description": "Idea 2 description",
+  "commentary": []
+},
+{
+  "id": "2",
+  "idea": "Test: Idea 3",
+  "description": "Idea 3 description",
+  "commentary": []
 }
-listToButons ();
+];
 
-//put an action on the item clicked
-function listToButonsAction (i) {
-    //indexClicked = i;
-    modal.style.display = "block";
-    //change with the new modal (jeremy)
-    document.getElementById("modTitle").innerHTML = dataJson[i].idea;
-    document.getElementById("modDescr").innerHTML = dataJson[i].description;
-    //document.getElementById("modCom").innerHTML = dataJson[indexClicked].commentary;
-    //alert(indexClicked+' has been clicked !');
-}
+// Au chargement de page j'affiche la liste
+window.addEventListener("load", ()=>{
+  displayIdeas();
+});
 
-//delete button PROBLEM with element zero
-document.getElementById("delete-button").onclick = function() {
-  //console.log(indexClicked);
-  //dataJson.splice(indexClicked , indexClicked);
-  delete dataJson[indexClicked];
-  //console.log(dataJson);
-  list ();
-  listToButons ();
-  modal.style.display = "none";
-}
+// When adding idea, reload the page
+document.querySelector(".add-idea").addEventListener("click", () => {
+  location.reload();
+})
+
+//-------------------------------------------------------------------------
+/*On affiche sur la page index la liste des idées
+on ajoute pour chaque idée un bouton delete edit et afficher */
+
+let displayIdeas = () => {
+  //decommenter pour nettoyer le localstorage
+  //localStorage.setItem('content', JSON.stringify(dataJson));
+  let listIdeas = localStorage.getItem('content') ? JSON.parse(localStorage.getItem('content')) : [];
+  let toDisplay = "";
+
+  // je parcours le tableau
+  // ----debut for
+  for (let i = 0; i<listIdeas.length; i++){
+    let li = document.createElement ("li");
+    li.innerText = listIdeas[i].idea;
+
+    //creation du bouton edit
+    let editBtn = document.createElement ("button");
+    editBtn.setAttribute("class", "modal-edit-btn");
+    editBtn.innerText = "Edit";
+
+    // function edit
+    editBtn.addEventListener('click', () => {
+      document.querySelector("#edit-title").value = listIdeas[i].idea;
+      document.querySelector("#edit-descr").innerText = listIdeas[i].description;
+      document.querySelector(".modal-com").innerText = listIdeas[i].commentary;
+
+      // on sauvegarde dans la local nos changement
+      let saveBtn = document.querySelector(".save-idea");
+      saveBtn.addEventListener("click", () => {
+        listIdeas[i].idea = document.querySelector("#edit-title").value;
+        listIdeas[i].description = document.querySelector("#edit-descr").value;
+        localStorage.setItem('content', JSON.stringify(listIdeas));
+        window.location.reload();
+      });// end of save
+
+      modalEdit.style.display = "block";
+
+    }); // end of edit
+
+
+    // creation du bouton delete
+    let deleteBtn = document.createElement ("button");
+    deleteBtn.setAttribute("id", "delete" + i);
+    deleteBtn.setAttribute("class", "modal-delete-btn");
+    deleteBtn.innerText = "Delete";
+
+    // function delete
+    deleteBtn.addEventListener('click', () => {
+      listIdeas.splice(i, 1);
+      localStorage.setItem('content', JSON.stringify(listIdeas));
+      window.location.reload();
+    }); // end of delete
+
+
+  /*Pour chaque idée, on doit pouvoir donner un
+  commentaire qui lui est propre*/
+    // creation du bouton Display
+    let displayBtn = document.createElement ("button");
+    displayBtn.setAttribute("class", "modal-display-btn");
+    displayBtn.innerText = "Display";
+
+
+    // function Display
+    displayBtn.addEventListener('click', () => {
+
+      // fonction add commentaire
+      let input_textarea = document.querySelector('.modal-comment');
+      let pComments = document.querySelector('.modal-com');
+      let addCommentBtn = document.querySelector('.add-comment');
+      let listIdeas = localStorage.getItem('content') ? JSON.parse(localStorage.getItem('content')) : [];
+      input_textarea.value="";
+
+      console.log(JSON.parse(localStorage.getItem('content')));
+
+      //function addComment pour ajouter
+      addCommentBtn.addEventListener('click', () => {
+        listIdeas[i].commentary.push(input_textarea.value);
+        localStorage.setItem('content', JSON.stringify(listIdeas));
+
+      }); //end addComment.btn
+
+      // on affiche tous les commentaires d'une idée
+      input_textarea.value="";
+      let toDisplay = "";
+      for (let j=0; j<listIdeas[i].commentary.length; j++ ){
+        toDisplay += "<p>" + listIdeas[i].commentary[j] + "</p>";
+      }// end of for
+
+      //listIdeas[i].commentary = document.querySelector(".modal-comment").value;
+      pComments.innerHTML = toDisplay;
+      modalComment.style.display = "block";
+  }); //end displayBtn
+
+    // on affiche la liste avec le ul et li +  avec les boutons display, delete et edit
+    let ul =document.querySelector('ul');
+    ul.appendChild(li);
+    ul.appendChild(deleteBtn);
+    ul.appendChild(btnEdit);
+    ul.appendChild(btnComment);
 
 
 
+  }; // end of for de la liste des idées
+};// en of displayIdeas-----------------
 
 
-/*
+/*Sur la page index, on doit pouvoir ajouter une nouvelle idée
+on a besoin de l'id de cette idée de sa value ainsi que sa description
+A chaque idée, on peut lui donner des commentaires*/
 
-let displayModalBox = (idTarget)=> {
-	let index = parseInt(idTarget.substr(4));
-	return idTarget;
-	console.log(index);
-};
+//Fonction ajoute idée, desciption, id
+//---------------------------------------------------
 
-//click buttons
-let prepareButton = () => {
-  if (listIdeas.hasChildNodes()) {
-    let children = listIdeas.childNodes;
-      for (let i = 0; i < children.length; i++) {
-        let temp = children[i].id;
-        document.getElementById(temp).onclick = function() {
-          displayModalBox(temp);
-        };
-    };
-  };
-};
-*/
+let newIdea = document.querySelector("#modal-title");
+let newDes = document.querySelector("#modal-descr");
+
+let addIdea = (i, d) =>{
+
+  let listIdeas = localStorage.getItem('content') ? JSON.parse(localStorage.getItem('content')) : [];
+
+  listIdeas.push({id: listIdeas.length ,idea: i.value,description: d.value,commentary: []});
+  localStorage.setItem('content', JSON.stringify(listIdeas));
+
+}// end of function addIdea
+
+// on ajoute une idée
+let addBtn =  document.querySelector(".add-idea");
+
+addBtn.addEventListener("click", () => {
+  addIdea(newIdea, newDes);
+
+});// end of add
+
+
+//--------------------------------------------------------------------------------------
+
+/*Pour chaque champs texte, on doit generer du markdown*/
+// -------------------------- Jeremy markdown convert to html--------------------------//
+
+// If you use require (Node etc), require as first the module and then create the instance
+let Remarkable = require('remarkable');
+// If you're in the browser, the Remarkable class is already available in the window
+let md = new Remarkable();
+
+document.querySelector(".add-idea").addEventListener("click", () => {
+  let text = (document.getElementById("modal-descr").value);
+  // document.getElementById("idea-descr").innerHTML = md.render(text);
+  console.log(md.render(text));
+})
--- script.js
+++ script.js
@@ -14,54 +14,55 @@ var modal = document.getElementById('modal-add');
 var modalEdit = document.getElementById('modal-edit');
 var modalComment = document.getElementById('modal-comment');
 // Get the button that opens the modal
-var btn = document.getElementById("modal-add-btn");
+var btn = document.querySelector("#modal-add-btn");
 var edit = document.querySelector('.modal-edit-btn');
 var display = document.querySelector('.modal-display-btn');
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal-add-close")[0];
+var span = document.getElementsByClassName("modal-close")[0];
+
+
+
+// When the user clicks the button X, close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  modalEdit.style.display = "none";
+  modalComment.style.display = "none";
+}
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+edit.onclick = function() {
+  modalEdit.style.display = "block";
 }
 
+display.onclick = function() {
+  modalComment.style.display = "block";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-
-
 }
-// Get the Edit modal
-var modal2 = document.getElementById('modal-edit');
-// Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("modal-edit-close")[0];
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
+  if (event.target == modalEdit) {
+    modalEdit.style.display = "none";
   }
 }
-
-// Get the Comment modal
-var modalComment = document.getElementById('modal-comment');
-// Get the <span> element that closes the modal
-var spanModalComment = document.getElementsByClassName("modal-comment-close")[0];
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modalComment) {
     modalComment.style.display = "none";
   }
 }
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+
 
 //-----------------------fin des fct de la modal box----------------------------------
 // debut des fonctions de l'application
